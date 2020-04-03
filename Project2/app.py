0d07dbfba96bde9081cda8236f78a7c6e5191106
Zee <gebengazizipho@gmail.com>
2020-04-03 21:23:59 +0200

    Zee's part

--- Project2/app.py
+++ Project2/app.py
@@ -9,6 +9,13 @@ import csv
 app = Flask(__name__)
 app.config.from_object(Config)
 
+#making lists we'll possibly need
+
+res_names = ["WhatABurger","Girl & The Goat","Vernick Food & Drink"]
+stars = [5,4,3,3,1]
+picts = ["../static/images/IMG-4316.JPG","../static/images/IMG-4317.JPG","../static/images/IMG-4318.JPG"]
+trading = ["24hrs","16:30 - 22:00","17:00 - 02:00"]
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -17,7 +24,7 @@ def index():
         flash('Form to be filled requested for user {},'.format(
             form.city.data))
         return redirect(url_for('index'))
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form,len = len(res_names),res_names = res_names,stars = stars,picts = picts,trading = trading)
     
 if __name__ == '__main__':
     app.run(debug=True)
