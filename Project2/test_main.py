f94330ddf0ea5d4e46cd8599ef6c1f137abb7a18
Thorin <lokimischieves@gmail.com>
2020-04-22 08:39:17 -0400

    might've found a way to test

--- Project2/test_main.py
+++ Project2/test_main.py
@@ -1,24 +1,14 @@
-import filecmp
-import json
+import unittest
 
-from py2neo import Graph
-from py2neo import Database
-
-graph = Graph("bolt://35.224.32.106:8000", auth=("neo4j", "yelpme"))
-
-def main():
-    print("--- test 1: checking if the restaurants returned is ordered by stars ---")
-    hold = get_buss("Champlain", "Burgers")
-    with open('tests/out/out1.json', 'w') as filehandle:
-        json.dump(hold, filehandle)
-    #problematic because it's a dictionary thingy that changes order
-    #cmp = filecmp.cmp("tests/out/out1.json", "tests/actual/actual1.json")
-    #print(cmp)
+from main import *
 
 
-def get_buss(city, cruisine):
-    store = graph.run("MATCH (m:Business)-[:IN_CATEGORY]->(n:Category) WHERE m.city=\""+city+"\" AND n.id=\""+cruisine+"\" RETURN m,n ORDER BY m.stars DESC, m.review_count DESC").data()
-    return store
-
-if __name__ == "__main__":
-    main()
+class TestGetBuss(unittest.TestCase):
+    # checks that the restaurants returned is in the required city 
+    # and serves the required cruisine
+    def test_buss(self):
+        hold = get_buss("Champlain", "Burgers")
+        for i in range(0, len(hold)-1):
+            print(hold[i]["m"])
+            self.assertAlmostEqual(hold[i]['m']['city'], "Champlain")
+            self.assertAlmostEqual(hold[i]['n']['id'], "Burgers")
