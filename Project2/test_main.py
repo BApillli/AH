48f15802c153469f195cf60b7cd17ee77f4268b9
Thorin <lokimischieves@gmail.com>
2020-04-29 05:39:56 -0400

    fixed first testcase

--- Project2/test_main.py
+++ Project2/test_main.py
@@ -3,28 +3,37 @@ import unittest
 from main import *
 
 
-class TestGetBuss(unittest.TestCase):
+class TestMain(unittest.TestCase):
     # checks that the restaurants returned is in the required city 
     # and serves the required cruisine
     def test_buss(self):
-        hold = get_buss("Champlain", "Burgers")
+        hold = get_buss("Scottsdale", "Hot Dogs")
+        #hold = get_buss("Champlain", "Burgers")
         sort = []
 
         # checks cruisine and city
         print("... Test to check whether it is in the required city and returns the required cruisine ...") 
         for i in range(0, len(hold)):
-            self.assertAlmostEqual(hold[i]['m']['city'], "Champlain")
-            self.assertAlmostEqual(hold[i]['n']['id'], "Burgers")
+            try:
+                self.assertAlmostEqual(hold[i]['m']['city'], "Scottsdale")
+            except ThatException:
+                self.fail("FAILED")
+            self.assertAlmostEqual(hold[i]['n']['id'], "Hot Dogs")
             sort.append(hold[i]['m']['stars'])
-        
+        print("PASSED")
         #checks if it is ordered by stars
+        print()
         print("... Test to check whether it is ordered by stars ...")    
-        
         sort1 = sort
         sort1 = sorted(sort1, reverse=True)
-        self.assertAlmostEqual(sort1, sort)
-        
+        try:
+            self.assertAlmostEqual(sort1, sort)
+            print("PASSED")
+        except ThatException:
+            self.fail("FAIL")
+
         #check if ties are broken
+        print()
         print("... Test to check if ties are broken ...") 
 
         hold2 = get_buss("Scottsdale", "Hot Dogs")
@@ -35,8 +44,23 @@ class TestGetBuss(unittest.TestCase):
             if hold2[i]['m']['stars'] == hold2[i+1]['m']['stars']:
                 a = hold2[i]['m']['review_count']
                 b = hold2[i+1]['m']['review_count']
-                #print("a is "+ str(a ) + " b is " + str(b))
-                self.assertGreaterEqual(a, b)
+                try:
+                    self.assertGreaterEqual(a, b)
+                except ThatException:
+                    self.fail("FAIL")
 
+    
+    def test_most_useful(self) :
+        print()
+        restaurant = get_buss("Scottsdale", "Hot Dogs")
+        review = most_useful(restaurant[0]['m']['id'])
+        print("Testing to see if the most useful review is returned")
+        print()
+        
+
+        # for i in range(0, len(restaurant)) :
+        #     sort.append(restaurant[i]['m']['stars'])
+        # sort1 = sorted(sort, reverse=True)
+        
         
             
